e3584fe567fdf1cf557b0707d0e19f1c13cc9557
YuxiaFu <111035310+YuxiaFu@users.noreply.github.com>
2025-09-01 15:10:44 +1000

    Update eval.sh

--- eval_tools/eval.sh
+++ eval_tools/eval.sh
@@ -1,7 +1,7 @@
-ckpt=results/ddd
+ckpt=results/phase1_ckpt
 phase=1
 python eval_tools/eval.py $ckpt $phase
 
-ckpt=results/ddd
+ckpt=results/phase2_ckpt
 phase=2
 python eval_tools/eval.py $ckpt $phase
